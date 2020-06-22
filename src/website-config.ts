c4298d3414fca4186e00ca976ada969bf3451626
amzbox <67238000+amzbox@users.noreply.github.com>
2020-06-22 10:11:03 +0700

    Add files via upload

---
+++ src/website-config.ts
@@ -0,0 +1,68 @@
+export interface WebsiteConfig {
+  title: string;
+  description: string;
+  coverImage?: string;
+  logo: string;
+  /**
+   * Specifying a valid BCP 47 language helps screen readers announce text properly.
+   * See: https://dequeuniversity.com/rules/axe/2.2/valid-lang
+   */
+  lang: string;
+  /**
+   * blog full path, no ending slash!
+   */
+  siteUrl: string;
+  /**
+   * full url, no username
+   */
+  facebook?: string;
+  /**
+   * full url, no username
+   */
+  twitter?: string;
+  /**
+   * hide or show all email subscribe boxes
+   */
+  showSubscribe: boolean;
+  /**
+   * create a list on mailchimp and then create an embeddable signup form. this is the form action
+   */
+  mailchimpAction?: string;
+  /**
+   * this is the hidden input field name
+   */
+  mailchimpName?: string;
+  /**
+   * name and id of the mailchimp email field
+   */
+  mailchimpEmailFieldName?: string;
+  /**
+  /**
+   * Meta tag for Google Webmaster Tools
+   */
+  googleSiteVerification?: string;
+  /**
+  /**
+   * Appears alongside the footer, after the credits
+   */
+  footer?: string;
+}
+
+const config: WebsiteConfig = {
+  title: 'AMZBox Blog - Update News, Tutorials & Tips Wordpress',
+  description: 'Update your tech news on my blog. And you can find tutorials and tips about wordpress blog',
+  coverImage: 'img/blog-cover.png',
+  logo: 'img/ghost-logo.png',
+  lang: 'en',
+  siteUrl: 'https://amzbox.netlify.app',
+  facebook: 'https://www.facebook.com/ghost',
+  twitter: 'https://twitter.com/tryghost',
+  showSubscribe: true,
+  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  mailchimpEmailFieldName: 'MERGE0',
+  googleSiteVerification: 'cyNZ3wWOr9SdzJb61lC_rE2E2PJvsTB1qt6uL9j1xSk',
+  footer: 'is based on Gatsby Casper',
+};
+
+export default config;
